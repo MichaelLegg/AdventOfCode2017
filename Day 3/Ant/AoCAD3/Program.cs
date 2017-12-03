595418c033f85c63c26e2fc4e39528a38ea76976
Ant Robinson <anthonyrobinson1@hotmail.co.uk>
2017-12-03 22:44:29 +0000

    Broken shit upload

--- Day 3/Ant/AoCAD3/Program.cs
+++ Day 3/Ant/AoCAD3/Program.cs
@@ -10,6 +10,106 @@ namespace AoCAD3
     {
         static void Main(string[] args)
         {
+            // Actually going to do todays... TODAY!!! OMG!!!
+            // FUCK THIS!
+            int[,] data = new int[101,101];
+            int counter = 1;
+            bool goLeft = false;
+            bool goUp = false;
+            int horTimes = 1;
+            int curHorTimes = horTimes;
+            int verTimes = 1;
+            int curVerTimes = verTimes;
+            int locationX = 51;
+            int locationY = 51;
+            bool turnVertical = true;
+
+            // initalize the array with all -1's
+            for (int x = 0; x < 101; x++)
+            {
+                for(int y = 0; y < 101; y++)
+                {
+                    data[x, y] = -1;
+                }
+            }
+            // Start adding the numbers!
+            
+            while (counter <= 10201)
+            {
+                data[locationX,locationY] = counter;
+                if(turnVertical == true)
+                {
+                    if (curHorTimes == 0)
+                    {
+                        turnVertical = !turnVertical;
+                        verTimes++;
+                        curVerTimes = verTimes;
+                        // start off horiz movement
+                        if (goLeft == true)
+                        {
+                            locationX--;
+                            curVerTimes--;
+                        }
+                        else
+                        {
+                            locationX++;
+                            curVerTimes--;
+                        }
+                    }
+                    else
+                    {
+                        // y up or down
+                        if (goUp == false)
+                        {
+                            locationY++;
+                            curHorTimes--;
+                        }
+                        else
+                        {
+                            locationY--;
+                            curHorTimes--;
+                        }
+                    }
+                }
+                else
+                {
+                    if (curVerTimes == 0)
+                    {
+                        turnVertical = !turnVertical;
+                        horTimes++;
+                        curHorTimes = horTimes;
+                        // Start vertical movement
+                        if (goUp == false)
+                        {
+                            locationY--;
+                            curHorTimes--;
+                        }
+                        else
+                        {
+                            locationY++;
+                            curHorTimes--;
+                        }
+                    }
+                    else
+                    {
+                        // y left or right
+                        if (goLeft == true)
+                        {
+                            locationX--;
+                            curVerTimes--;
+                        }
+                        else
+                        {
+                            locationX++;
+                            curVerTimes--;
+                        }
+                    }
+                }
+                counter++;
+            }
+
+            Console.WriteLine("all done!");
+            Console.ReadLine();
         }
     }
 }
