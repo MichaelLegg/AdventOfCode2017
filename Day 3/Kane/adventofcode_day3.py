1b0cd94efcf7139da431039781fff0abf9b95914
Kane Small <kanesmall@outlook.com>
2017-12-04 23:34:20 +0000

    Day 3, Parts 1 & 2 Completed

--- Day 3/Kane/adventofcode_day3.py
+++ Day 3/Kane/adventofcode_day3.py
@@ -119,4 +119,177 @@ def spiral_grid():
 
     print sum(sums)
 
-spiral_grid()
+def spiral_grid_2():
+
+    input = 312051
+    grid_size = int(math.ceil(math.sqrt(input)))
+
+    list = [[0 for _ in range(grid_size)] for _ in range(grid_size)]
+
+    centre = int(math.floor(grid_size / 2))
+    y = centre
+    x = centre
+
+    length = len(list) - 1
+
+    count = 1
+
+    for i in range(input):
+        if(x == length and y != length):
+            N = 0
+            NE = 0
+            E = 0
+            SE = 0
+            S = list[y + 1][x]
+            SW = list[y + 1][x - 1]
+            W = list[y][x - 1]
+            NW = list[y - 1][x - 1]
+
+        elif(x == length and y == 0):
+            N = 0
+            NE = 0
+            E = 0
+            SE = 0
+            S = list[y + 1][x]
+            SW = list[y + 1][x - 1]
+            W = 0
+            NW = 0
+
+        elif(x != length and y == 0):
+            N = 0
+            NE = 0
+            E = list[y][x + 1]
+            SE = list[y + 1][x + 1]
+            S = list[y + 1][x]
+            SW = list[y + 1][x - 1]
+            W = 0
+            NW = 0
+
+        elif (x == 0 and y == 0):
+            N = 0
+            NE = 0
+            E = list[y][x + 1]
+            SE = list[y + 1][x + 1]
+            S = 0
+            SW = 0
+            W = 0
+            NW = 0
+
+        elif (x == 0 and y != length):
+            N = list[y - 1][x]
+            NE = list[y - 1][x + 1]
+            E = list[y][x + 1]
+            SE = list[y + 1][x + 1]
+            S = 0
+            SW = 0
+            W = 0
+            NW = 0
+
+        elif (x == 0 and y == length):
+            N = list[y - 1][x]
+            NE = list[y - 1][x + 1]
+            E = 0
+            SE = 0
+            S = 0
+            SW = 0
+            W = 0
+            NW = 0
+
+        elif (x == length and y == length):
+            N = list[y - 1][x]
+            NE = 0
+            E = 0
+            SE = 0
+            S = 0
+            SW = 0
+            W = list[y][x - 1]
+            NW = list[y - 1][x - 1]
+
+        elif (x != 0 and y == length):
+            N = list[y - 1][x]
+            NE = list[y - 1][x + 1]
+            E = 0
+            SE = 0
+            S = 0
+            SW = 0
+            W = list[y][x - 1]
+            NW = list[y - 1][x - 1]
+
+        else:
+            N = list[y - 1][x]
+            NE = list[y - 1][x + 1]
+            E = list[y][x + 1]
+            SE = list[y + 1][x + 1]
+            S = list[y + 1][x]
+            SW = list[y + 1][x - 1]
+            W = list[y][x - 1]
+            NW = list[y - 1][x - 1]
+
+        if (N == 0 and E == 0 and S == 0 and W == 0):
+            list[y][x] = count
+            x = x + 1
+        elif (N == 0 and E == 0 and S == 0 and W != 0):
+            list[y][x] = N + NE + E + SE + S + SW + W + NW
+            if(list[y][x] > input):
+                exit
+            else:
+                y = y - 1
+        elif (N == 0 and E != 0 and S == 0 and W != 0):
+            list[y][x] = N + NE + E + SE + S + SW + W + NW
+            if (list[y][x] > input):
+                exit
+            else:
+                y = y - 1
+        elif (N != 0 and E != 0 and S == 0 and W == 0):
+            list[y][x] = N + NE + E + SE + S + SW + W + NW
+            if (list[y][x] > input):
+                exit
+            else:
+                y = y + 1
+        elif (N != 0 and E == 0 and S == 0 and W != 0):
+            list[y][x] = N + NE + E + SE + S + SW + W + NW
+            if (list[y][x] > input):
+                exit
+            else:
+                x = x + 1
+        elif (N == 0 and E == 0 and S != 0 and W == 0):
+            list[y][x] = N + NE + E + SE + S + SW + W + NW
+            if (list[y][x] > input):
+                exit
+            else:
+                x = x - 1
+        elif (N == 0 and E != 0 and S != 0 and W == 0):
+            list[y][x] = N + NE + E + SE + S + SW + W + NW
+            if (list[y][x] > input):
+                exit
+            else:
+                x = x - 1
+        elif (N != 0 and E == 0 and S == 0 and W != 0):
+            list[y][x] = N + NE + E + SE + S + SW + W + NW
+            if (list[y][x] > input):
+                exit
+            else:
+                x = x + 1
+        elif (N == 0 and E != 0 and S == 0 and W == 0):
+            list[y][x] = N + NE + E + SE + S + SW + W + NW
+            if (list[y][x] > input):
+                exit
+            else:
+                y = y + 1
+        elif (N != 0 and E == 0 and S == 0 and W == 0):
+            list[y][x] = N + NE + E + SE + S + SW + W + NW
+            if (list[y][x] > input):
+                exit
+            else:
+                x = x + 1
+        elif (N == 0 and E == 0 and S != 0 and W != 0):
+            list[y][x] = N + NE + E + SE + S + SW + W + NW
+            if (list[y][x] > input):
+                exit
+            else:
+                y = y - 1
+
+    print list[y][x]
+
+# spiral_grid()
+spiral_grid_2()
