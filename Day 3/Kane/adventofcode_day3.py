3df6196655261f9751e1cf106b699e9da29ee043
Kane Small <kanesmall@outlook.com>
2017-12-04 22:26:31 +0000

    Day 3, Part 1 Completed

--- Day 3/Kane/adventofcode_day3.py
+++ Day 3/Kane/adventofcode_day3.py
@@ -1,71 +1,122 @@
-def grid():
+import math
 
-    list = [[0 for _ in range(11)] for _ in range(11)]
+def find_item(list, item):
+    return [[i, list[i].index(item)] for i in range(len(list)) if item in list[i]]
 
-    y = 5
-    x = 5
+def spiral_grid():
+
+    input = 312051
+    grid_size = int(math.ceil(math.sqrt(input)))
+
+    list = [[0 for _ in range(grid_size)] for _ in range(grid_size)]
+
+    centre = int(math.floor(grid_size / 2))
+    y = centre
+    x = centre
+
+    length = len(list) - 1
 
     count = 1
 
-    for i in range(121):
-        if(x + 1 < 10 and y + 1 < 10):
+    for i in range(input):
+        if(x == length and y != length):
+            N = 0
+            E = 0
+            S = list[y + 1][x]
+            W = list[y][x - 1]
+
+        elif(x == length and y == 0):
+            N = 0
+            E = 0
+            S = list[y + 1][x]
+            W = 0
+
+        elif(x != length and y == 0):
+            N = 0
+            E = list[y][x + 1]
+            S = list[y + 1][x]
+            W = 0
+
+        elif (x == 0 and y == 0):
+            N = 0
+            E = list[y][x + 1]
+            S = 0
+            W = 0
+
+        elif (x == 0 and y != 0):
+            N = list[y - 1][x]
+            E = list[y][x + 1]
+            S = 0
+            W = 0
+
+        elif (x == 0 and y == length):
+            N = list[y - 1][x]
+            E = 0
+            S = 0
+            W = 0
+
+        elif (x != 0 and y == length):
+            N = list[y - 1][x]
+            E = 0
+            S = 0
+            W = list[y][x - 1]
+
+        else:
             N = list[y - 1][x]
             E = list[y][x + 1]
             S = list[y + 1][x]
             W = list[y][x - 1]
 
-            if (N == 0 and E == 0 and S == 0 and W == 0):
-                list[y][x] = count
-                x = x + 1
-            elif (N == 0 and E == 0 and S == 0 and W != 0):
-                list[y][x] = count
-                y = y - 1
-            elif (N == 0 and E != 0 and S == 0 and W != 0):
-                list[y][x] = count
-                y = y - 1
-            elif (N != 0 and E != 0 and S == 0 and W == 0):
-                list[y][x] = count
-                y = y + 1
-            elif (N != 0 and E == 0 and S == 0 and W != 0):
-                list[y][x] = count
-                x = x + 1
-            elif (N == 0 and E == 0 and S != 0 and W == 0):
-                list[y][x] = count
-                x = x - 1
-            elif (N == 0 and E != 0 and S != 0 and W == 0):
-                list[y][x] = count
-                x = x - 1
-            elif (N != 0 and E == 0 and S == 0 and W != 0):
-                list[y][x] = count
-                x = x + 1
-            elif (N == 0 and E != 0 and S == 0 and W == 0):
-                list[y][x] = count
-                y = y + 1
-            elif (N != 0 and E == 0 and S == 0 and W == 0):
-                list[y][x] = count
-                x = x + 1
-            elif (N == 0 and E == 0 and S != 0 and W != 0):
-                list[y][x] = count
-                y = y - 1
-        else:
-            if (x == 0 and y != 0):
-                list[y][x] = count
-                x = x - 1
-
-            elif (x == 10):
-                list[y][x] = count
-                y = y - 1
-
-            elif (x == 0 and y != 10):
-                list[y][x] = count
-                y = y + 1
-
-            elif (y == 10):
-                list[y][x] = count
-                x = x - 1
+        if (N == 0 and E == 0 and S == 0 and W == 0):
+            list[y][x] = count
+            x = x + 1
+        elif (N == 0 and E == 0 and S == 0 and W != 0):
+            list[y][x] = count
+            y = y - 1
+        elif (N == 0 and E != 0 and S == 0 and W != 0):
+            list[y][x] = count
+            y = y - 1
+        elif (N != 0 and E != 0 and S == 0 and W == 0):
+            list[y][x] = count
+            y = y + 1
+        elif (N != 0 and E == 0 and S == 0 and W != 0):
+            list[y][x] = count
+            x = x + 1
+        elif (N == 0 and E == 0 and S != 0 and W == 0):
+            list[y][x] = count
+            x = x - 1
+        elif (N == 0 and E != 0 and S != 0 and W == 0):
+            list[y][x] = count
+            x = x - 1
+        elif (N != 0 and E == 0 and S == 0 and W != 0):
+            list[y][x] = count
+            x = x + 1
+        elif (N == 0 and E != 0 and S == 0 and W == 0):
+            list[y][x] = count
+            y = y + 1
+        elif (N != 0 and E == 0 and S == 0 and W == 0):
+            list[y][x] = count
+            x = x + 1
+        elif (N == 0 and E == 0 and S != 0 and W != 0):
+            list[y][x] = count
+            y = y - 1
         count = count + 1
 
-    for row in list:
-        print row
+    inputVal = find_item(list, input)
+    start = [centre, centre]
 
-grid()
+    sums = []
+
+    if(start[0] > inputVal[0][0]):
+        sums.append(start[0] - inputVal[0][0])
+    else:
+        sums.append(inputVal[0][0] - start[0])
+
+    if (start[1] > inputVal[0][1]):
+        sums.append(start[1] - inputVal[0][1])
+    else:
+        sums.append(inputVal[0][1] - start[1])
+
+    print sum(sums)
+
+spiral_grid()
