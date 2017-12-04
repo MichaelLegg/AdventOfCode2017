ad4b273198b2a042cbfd1dad901baa76e5390b9a
Michael Legg <michael.legg@outlook.com>
2017-12-04 04:03:33 +0000

    day 3 part 2 done...never write code like this

--- Day 3/Mikey/AoCMD3/Program.cs
+++ Day 3/Mikey/AoCMD3/Program.cs
@@ -13,9 +13,7 @@ namespace AoCMD3
             const int BIGGESTNUMBER = 361527;
             BuildArray(BIGGESTNUMBER);
 
-
-
-
+            Console.WriteLine("Puzzel 2 output: " + Puzzle2(BIGGESTNUMBER));
             Console.ReadLine();
         }
 
@@ -94,7 +92,7 @@ namespace AoCMD3
                     currentValue++;
                 
             } while (!maxSetFlag); // do it until we've set the biggest value
-            Console.WriteLine(Puzzle1(builtArray, dimension, biggestNumber));
+            Console.WriteLine("Puzzle 1 solution: " + Puzzle1(builtArray, dimension, biggestNumber));
         }
         
         private static int Puzzle1(int[,] builtArray, int dimension, double searchVal)
@@ -118,10 +116,126 @@ namespace AoCMD3
             // quick and dirty
             if (searchX > half)
                 sum += searchX - half;
-            else       
-            
-           (searchX - half) + (half -searchY);
+            else
+                sum += half - searchX;
+
+            if (searchY > half)
+                sum += searchY - half;
+            else
+                sum += half - searchY;
+           
             return sum;
+        }
+
+
+        // THIS IS JUST THE WORST CODE EVER. JUST REBUILD THE FIRST ONE, BUT ADD THE VALUES AROUND EACH SQUARE
+        private static double Puzzle2(double biggestNumber)
+        {
+            int dimension = (int)Math.Ceiling(Math.Sqrt(biggestNumber));
+            dimension--; // since it's going to be 0 indexed
+
+            // check if it's even or not
+            if ((dimension % 2) == 0)             
+                dimension++;            
+
+            int[,] builtArray = new int[dimension, dimension];
+
+            int half = dimension / 2;
+
+            int currentX = half;
+            int currentY = half;            
+
+            bool maxSetFlag = false;
+
+            State currentState = State.increasingX;
+
+            // OK X AND Y ARE ACTUALLY THE WRONG WAY AROUND, IT'S ROWS AND COLUMNS RATHER THAN POSITIONS ON AN AXIS SO VARIABLE NAMES ARE WROOOOONG
+            // ALSO, I PROBABLY REALLY DIDN'T NEED TO ACTUALLY BUILD A SPIRAL TO SOLVE THIS, BUT IT'S QUITE COOL AND NOW I KNOW HOW TO BUILD A SPIRAL...RIGHT?
+            do
+            {
+                int toBeAdded = 0;
+                // first one
+                if (currentX == half && currentY == half)
+                {
+                    builtArray[currentX, currentY] = 1;
+                }
+                else
+                {
+                    // EAST
+                    if (currentX + 1 < dimension - 1)
+                        toBeAdded += builtArray[currentX + 1, currentY];
+
+                    // WEST
+                    if (currentX - 1 > 0)
+                        toBeAdded += builtArray[currentX - 1, currentY];
+
+                    // NORTH
+                    if (currentY - 1 > 0)
+                        toBeAdded += builtArray[currentX, currentY - 1];
+
+                    // SOUTH
+                    if (currentY + 1 < dimension - 1)
+                        toBeAdded += builtArray[currentX, currentY + 1];
+
+                    // NORTH EAST
+                    if (currentX + 1 < dimension - 1 && currentY - 1 > 0)
+                        toBeAdded += builtArray[currentX + 1, currentY - 1];
+
+                    // NORTH WEST
+                    if (currentX - 1 > 0 && currentY - 1 > 0)
+                        toBeAdded += builtArray[currentX - 1, currentY - 1];
+
+                    // SOUTH EAST
+                    if (currentX + 1 < dimension - 1 && currentY + 1 < dimension - 1)
+                        toBeAdded += builtArray[currentX + 1, currentY + 1];
+
+                    // SOUTH WEST      
+                    if (currentX - 1 > 0 && currentY + 1 < dimension)
+                        toBeAdded += builtArray[currentX - 1, currentY + 1];
+                    builtArray[currentX, currentY] = toBeAdded;
+
+                }
+
+                Console.WriteLine(toBeAdded);
+                // check here for when we've hit one where it's bigger
+                if (toBeAdded > biggestNumber)
+                    return toBeAdded;
+                
+
+                    if (currentState == State.increasingX)
+                {
+                    currentX++;
+                    if (currentX + 1 > dimension - 1 || (builtArray[currentX + 1, currentY] == 0) && builtArray[currentX, currentY - 1] == 0)
+                    {
+                        currentState = State.decreasingY;
+                    }
+                }
+                else if (currentState == State.increasingY)
+                {
+                    currentY++;
+                    if (currentY + 1 > dimension - 1 || (builtArray[currentX, currentY + 1] == 0) && builtArray[currentX + 1, currentY] == 0)
+                    {
+                        currentState = State.increasingX;
+                    }
+                }
+                else if (currentState == State.decreasingX)
+                {
+                    currentX--;
+                    if (currentX - 1 < 0 || (builtArray[currentX - 1, currentY] == 0) && builtArray[currentX, currentY + 1] == 0)
+                    {
+                        currentState = State.increasingY;
+                    }
+                }
+                else if (currentState == State.decreasingY)
+                {
+                    currentY--;
+                    if (currentY - 1 < 0 || (builtArray[currentX, currentY - 1] == 0) && builtArray[currentX - 1, currentY] == 0)
+                    {
+                        currentState = State.decreasingX;
+                    }
+                }              
+            } while (!maxSetFlag); // do it until we've set the biggest value
+            return 1;
         }
     }
 }
