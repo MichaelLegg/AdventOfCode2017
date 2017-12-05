6c599324f4705e83be16f3d3c60dd5eb3dee60c0
Kane Small <kanesmall@outlook.com>
2017-12-05 02:53:07 +0000

    Not quite there yet with Part 1...

--- Day 4/Kane/adventofcode_day4.py
+++ Day 4/Kane/adventofcode_day4.py
@@ -1,6 +1,21 @@
 def noDupes():
+    count = 0
     with open('input.txt') as f:
         for line in f:
-            line = map(int, line.split("\t"))
+            line = map(str, line.split(" "))
+            bool = False
+            for i in range(len(line)):
+                for j in range(i):
+                    line[i].rstrip('\n')
+                    line[j].rstrip('\n')
+                    if(line[i] == line[j]):
+                        # print line[i], line[j]
+                        bool = True
+                        break
+            if(bool == False):
+                # print line
+                count += 1
+
+    print count
 
 noDupes()
