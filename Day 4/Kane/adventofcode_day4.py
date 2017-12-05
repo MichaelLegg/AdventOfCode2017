847a770268d2a0b924da37bc90dee46fecacaeeb
kanesmall <kanesmall@outlook.com>
2017-12-05 16:47:25 +0000

    Day 4, Part 1 Completed

--- Day 4/Kane/adventofcode_day4.py
+++ Day 4/Kane/adventofcode_day4.py
@@ -1,21 +1,16 @@
 def noDupes():
     count = 0
-    with open('test.txt') as f:
+    with open('input.txt') as f:
         for line in f:
             line = map(str, line.split(" "))
-            print line
             bool = False
             for i in range(len(line)):
                 for j in range(i+1,len(line)):
-                    print i,j
+                    line[i] = line[i].rstrip('\n')
+                    line[j] = line[j].rstrip('\n')
                     if(line[i] == line[j]):
-                        print line[i], line[j]
-                        print count
                         bool = True
                         break
-                if(bool == True):
-                    # print line
-                    break
             if(bool == False):
                 count += 1
 
