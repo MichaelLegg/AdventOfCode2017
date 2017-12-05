ca41289fd0743a9141b8bbec03ba1316d09ec555
kanesmall <kanesmall@outlook.com>
2017-12-05 17:19:29 +0000

    Day 4, Parts 1 & 2 Completed. Day 4 README.md updated.

--- Day 4/Kane/adventofcode_day4.py
+++ Day 4/Kane/adventofcode_day4.py
@@ -16,4 +16,45 @@ def noDupes():
 
     print count
 
-noDupes()
+def anagram():
+    count = 0
+    with open('input.txt') as f:
+        for line in f:
+            line = map(str, line.split(" "))
+            bool = False
+            for i in range(len(line)):
+                for j in range(i + 1, len(line)):
+                    line[i] = line[i].rstrip('\n')
+                    line[j] = line[j].rstrip('\n')
+
+                    wordi = []
+                    wordj = []
+                    for ch in line[i]:
+                        wordi += ch
+
+                    for ch in line[j]:
+                        wordj += ch
+
+                    wordi.sort()
+                    wordj.sort()
+
+                    isorted = ""
+                    jsorted = ""
+
+                    for ch in wordi:
+                        isorted += ch
+
+                    for ch in wordj:
+                        jsorted += ch
+
+                    if(isorted == jsorted):
+                        bool = True
+                        break
+            if (bool == False):
+                count += 1
+
+    print count
+
+# noDupes()
+anagram()
+
