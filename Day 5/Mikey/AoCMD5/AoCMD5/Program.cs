08a2515b622d77ed3319bd914b4a9b4e1f212ddf
Michael Legg <michael.legg@outlook.com>
2017-12-06 02:54:15 +0000

    day 5 part 2 and part 2 without breaking the stack

--- Day 5/Mikey/AoCMD5/AoCMD5/Program.cs
+++ Day 5/Mikey/AoCMD5/AoCMD5/Program.cs
@@ -13,33 +13,66 @@ namespace AoCMD5
         static void Main(string[] args)
         {
             string[] lines = File.ReadAllLines(@"..\..\input.txt");
-
             int[] inputArray = new int[lines.Length];
             inputArray = Array.ConvertAll(lines, int.Parse);
 
-            Thread th = new Thread(() =>
-            {
-                Console.WriteLine(Puzzle1(inputArray, 0, 0));
-            },
-            0x10000000);
+            Thread th = new Thread(() => { Console.WriteLine(Puzzle1(inputArray, 0, 0));}, 0x10000000);
             th.Start();
             th.Join();
+            inputArray = Array.ConvertAll(lines, int.Parse);
+            //Thread th2 = new Thread(() => { Console.WriteLine(Puzzle2(inputArray, 0, 0)); }, 0x10000000);
+            //th2.Start();
+            //th2.Join();
+
+            Console.WriteLine(Puzzle2LessStackOverflow(inputArray));
             Console.ReadLine();
         }
 
-        private static double Puzzle1(int[] inputArray, int currentIndex, double jumps)
-        {
-            if (currentIndex + inputArray[currentIndex] >= 0 && inputArray[currentIndex] + currentIndex < inputArray.Length)
-            {
+        private static double Puzzle1(int[] inputArray, int currentIndex, double jumps){
+            if (currentIndex + inputArray[currentIndex] >= 0 && inputArray[currentIndex] + currentIndex < inputArray.Length){
                 int next = inputArray[currentIndex];
                 inputArray[currentIndex]++;
                 jumps++;
                 return (Puzzle1(inputArray, (currentIndex + next), jumps));
-            }
-            return jumps + 1;
+            }else
+                return jumps + 1;
         }
+
+        private static double Puzzle2LessStackOverflow(int[] inputArray)
+        {
+            int currentIndex = 0;
+            double jumps = 0;
+            while (currentIndex + inputArray[currentIndex] >= 0 && inputArray[currentIndex] + currentIndex < inputArray.Length)
+            {
+                int next = inputArray[currentIndex];
+                inputArray[currentIndex] += (inputArray[currentIndex] > 2) ? -1 : 1;
+                jumps++;
+                currentIndex += next;
+            }
+            return jumps+1;
+        }
+
+
+
+        //private static double Puzzle2(int[] inputArray, int currentIndex, double jumps)
+        //{
+        //    if (currentIndex + inputArray[currentIndex] >= 0 && inputArray[currentIndex] + currentIndex < inputArray.Length)
+        //    {
+        //        int next = inputArray[currentIndex];
+        //        inputArray[currentIndex] += (inputArray[currentIndex] > 2) ? -1 : 1;
+        //        jumps++;
+        //        return (Puzzle2(inputArray, (currentIndex + next), jumps));
+        //    }
+        //    else
+        //        return jumps + 1;
+        //}
+
+
     }
 }
+
+
+
 
     
 
