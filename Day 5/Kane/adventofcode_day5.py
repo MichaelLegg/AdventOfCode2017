fbf239cd1362fcb6dd5ed5f804f01f7612c77172
Michael Legg <michael.legg@outlook.com>
2017-12-06 01:14:42 +0000

    Revert "Merge branch 'master' of https://github.com/MichaelLegg/AdventOfCode2017"
    This reverts commit 4a2da309ca82e9dc66fe89a37263b5acdb3ed4b3, reversing
    changes made to e30a1a5f4e87965145b80aea0dc847efade993fd.

--- Day 5/Kane/adventofcode_day5.py
+++ Day 5/Kane/adventofcode_day5.py
@@ -1,4 +1,4 @@
-def maze_p1():
+def maze():
     ints = []
     with open('input.txt') as f:
         for i in f:
@@ -29,42 +29,4 @@ def maze_p1():
 
     print count
 
-def maze_p2():
-    ints = []
-    with open('input.txt') as f:
-        for i in f:
-            ints.append(int(i.rstrip('\n')))
-
-    exit = True
-    idx = 0
-    last = len(ints) - 1
-    count = 0
-
-    while(exit):
-        if(idx >= 0 and idx <= last):
-            if(ints[idx] > 0):
-                if(ints[idx] >= 3):
-                    temp = ints[idx]
-                    ints[idx] -= 1
-                    idx += temp
-                    count += 1
-                else:
-                    temp = ints[idx]
-                    ints[idx] += 1
-                    idx += temp
-                    count += 1
-            elif(ints[idx] < 0):
-                temp = ints[idx]
-                ints[idx] += 1
-                idx += temp
-                count += 1
-            else:
-                ints[idx] += 1
-                count += 1
-        else:
-            exit = False
-
-    print count
-
-# maze_p1()
-maze_p2()
+maze()
--- README.md
+++ README.md
@@ -1,10 +1,3 @@
 # AdventOfCode2017
-A group repository for the 2017 AdventOfCode (http://adventofcode.com/2017).
-
-Advent of Code is a series of small programming puzzles for a variety of skill levels. They are self-contained and are just as appropriate for an expert who wants to stay sharp as they are for a beginner who is just learning to code. Each puzzle calls upon different skills and has two parts that build on a theme.
-
-Stats Page: http://adventofcode.com/2017/stats
-
-Private Leaderboard: http://adventofcode.com/2017/leaderboard/private/view/225069
-
-Subreddit: https://www.reddit.com/r/adventofcode/
+A group repository for the 2017 AdventOfCode.
+http://adventofcode.com/2017
